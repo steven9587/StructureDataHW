b37246d6df03b7521b16e6fe22a14d5bdd7128f0
steven9587 <steven9587@gmail.com>
2018-11-10 16:53:01 +0800

    HW2-2

---
+++ src/hw2/GCD.java
@@ -0,0 +1,40 @@
+package hw2;
+
+import java.util.Scanner;
+
+public class GCD {
+	public static int a;
+	public static int b;
+
+	public static void main(String[] args) {
+		// 輸入&讀取資料
+		System.out.print("請輸入兩數A，B：");
+		Scanner scanner = new Scanner(System.in);
+		String line = scanner.nextLine();
+		String[] tokens = line.split(",");
+		a = Integer.parseInt(tokens[0]);
+		b = Integer.parseInt(tokens[1]);
+		// 輾轉相除法
+		while (a != 0 && b != 0) {
+			GCD(a, b);
+		}
+		// 印出答案
+		if (a == 0) {
+			System.out.println("GCD = " + b);
+		} else {
+			System.out.println("GCD = " + a);
+		}
+
+	}
+
+	// 遞迴方法
+	public static int GCD(int number1, int number2) {
+		if (a > b) {
+			a = a - b;
+			return a;
+		} else {
+			b = b - a;
+			return b;
+		}
+	}
+}
--- src/hw2/NFactorial.java
+++ src/hw2/NFactorial.java
@@ -3,31 +3,34 @@ package hw2;
 import java.util.Scanner;
 
 public class NFactorial {
-	
+
 	public static int answer = 1;
 
 	public static void main(String[] args) {
+		// 輸入&讀取資料
 		System.out.print("清輸入階層數(n):");
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		if(n<0) {
+		if (n < 0) {
 			System.out.println("輸入錯誤,請輸入>=0的數!");
-		}else {
+		} else {
 			while (n >= 0) {
-			if (n == 0) {
-				answer = answer * 1;
-				n--;
-			} else {
-				answer = factorial(n);
-				n--;
+				// 階層運算
+				if (n == 0) {
+					answer = answer * 1;
+					n--;
+				} else {
+					answer = factorial(n);
+					n--;
+				}
 			}
-		}
-		System.out.println("答案："+answer);
+			System.out.println("答案：" + answer);
 		}
 	}
-	
+
+	// 遞迴方法
 	public static int factorial(int n) {
-		answer = answer * n ;
+		answer = answer * n;
 		return answer;
 	}
 }
