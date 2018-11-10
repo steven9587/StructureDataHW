9e62a96a834178182241ba8266626ac229bc822e
steven9587 <steven9587@gmail.com>
2018-11-10 16:05:50 +0800

    HW2-1

--- src/hw2/NFactorial.java
+++ src/hw2/NFactorial.java
@@ -10,7 +10,10 @@ public class NFactorial {
 		System.out.print("清輸入階層數(n):");
 		Scanner scanner = new Scanner(System.in);
 		int n = scanner.nextInt();
-		while (n >= 0) {
+		if(n<0) {
+			System.out.println("輸入錯誤,請輸入>=0的數!");
+		}else {
+			while (n >= 0) {
 			if (n == 0) {
 				answer = answer * 1;
 				n--;
@@ -20,6 +23,7 @@ public class NFactorial {
 			}
 		}
 		System.out.println("答案："+answer);
+		}
 	}
 	
 	public static int factorial(int n) {
